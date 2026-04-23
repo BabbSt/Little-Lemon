3776568e6f45429c8df6c81ba04ef77ee37d13b7
BabbSt <step980@gmail.com>
2026-04-23 09:49:13 -0600

    Built Components & Meta data
    Built components for header, nav, main, and footer. Added content to all but main.
    
    Added meta data for open graph protocol in index.html
    
    Following instructions from course 8 module 2

---
+++ little-lemon/src/components/Footer.js
@@ -0,0 +1,28 @@
+function Footer(){
+    return (<footer>
+    <nav>
+        <h2>Doormat Navigation</h2>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>Order Online</a></li>
+            <li><a>Login</a></li>
+        </ul>
+    </nav>
+    <h2>Contact</h2>
+    <ul>
+        <li>Address</li>
+        <li>Phone</li>
+        <li>Email</li>
+    </ul>
+    <h2>Social Media Links</h2>
+    <ul>
+        <li><a>Instagram</a></li>
+        <li><a>Facebook</a></li>
+    </ul>
+    </footer>);
+}
+
+export default Footer;
---
+++ little-lemon/src/components/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+    <header>
+        <img src="./public/Logo.svg" alt="Little Lemon Logo"/>
+    </header>
+    );
+}
+
+export default Header;
